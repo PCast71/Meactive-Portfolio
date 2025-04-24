976560c86a883c38ccaeed2a7f8b76cc15becae6
PCast71 <patrickc77hhs@gmail.com>
2025-04-24 16:19:33 -0600

    refreshed emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,11 +33,12 @@ const Contact = () => {
       };
 
       emailjs.send(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-        templateParams,
-        process.env.REACT_APP_EMAILJS_USER_ID
-      )
+  process.env.REACT_APP_EMAILJS_SERVICE_ID,
+  process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  templateParams,
+  process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+)
+
         .then((response) => {
           setStatus('Message sent successfully!');
           setIsSending(false);
